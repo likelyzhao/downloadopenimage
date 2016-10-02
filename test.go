fdebc9aaa708e08f4770cc384e8d76ce86ba7d53
zhao zhijian <likely_zhao@163.com>
2016-10-02 22:45:18 +0800

    fix panic error

--- test.go
+++ test.go
@@ -7,9 +7,10 @@ func main() {
 	//testos()
 	//var res
 	var filename = "E:\\Research\\open Image\\images_2016_08_v2\\validation\\images.csv"
-	res := testos.TestfileOpen(filename)
+	//res := testos.TestfileOpen(filename)
+	res := loadInfos.LoadingOpenImageInfo(filename)
 	//	download.TestMain()
 
-	fmt.Printf("openfile_flag= %d\n", res)
+	fmt.Printf("openfile_flag= %d\n", len(res))
 	fmt.Println("test")
 }
--- testos/testos.go
+++ testos/testos.go
@@ -1,4 +1,4 @@
-package testos
+package loadInfos
 
 import (
 	"bufio"
@@ -8,8 +8,54 @@ import (
 	"strings"
 )
 
-//import "os"
+type OpenImageInfo struct {
+	fileIdx   string
+	fileUrl   string
+	fileTitle string
+}
 
+//import "os"
+func LoadingOpenImageInfo(filePath string) (infos []OpenImageInfo) {
+	//infosS := make([]OpenImageInfo, 1, 10)
+	f, err := os.OpenFile(filePath, os.O_RDONLY, 0660)
+	if err != nil {
+		panic(err)
+	}
+	// stuct inputs
+	bfRd := bufio.NewReader(f)
+	line, err := bfRd.ReadString('\n')
+	if err != nil { //遇到任何错误立即返回，并忽略 EOF 错误信息
+		if err == io.EOF {
+			panic(err)
+		}
+		panic(err)
+	}
+	splits := strings.Split(line, ",")
+	for i := 0; i < len(splits); i++ {
+		fmt.Printf("%s\n", splits[i])
+	}
+
+	for {
+		line, err := bfRd.ReadString('\n')
+		if err != nil { //遇到任何错误立即返回，并忽略 EOF 错误信息
+			if err == io.EOF {
+				f.Close()
+				return
+			}
+			panic(err)
+		}
+		splits := strings.Split(line, ",")
+		infos = append(infos, OpenImageInfo{splits[0], splits[1], splits[len(splits)-1]})
+
+		defer func() {
+			f.Close()
+			return
+		}()
+	}
+
+}
+
+/*
 func TestfileOpen(filePath string) (flag int) {
 	flag = 0
 	f, err := os.OpenFile(filePath, os.O_RDONLY, 0660)
@@ -33,10 +79,21 @@ func TestfileOpen(filePath string) (flag int) {
 		fmt.Printf("%s\n", splits[i])
 	}
 
+	for {
+
+		line, err := bfRd.ReadString('\n')
+		if err != nil { //遇到任何错误立即返回，并忽略 EOF 错误信息
+			if err == io.EOF {
+				return
+			}
+			return
+		}
+	}
+
 	defer f.Close()
 	return flag
 }
-
+*/
 func Add(num1 int, num2 int) (result int) {
 	return num1 + num2
 
